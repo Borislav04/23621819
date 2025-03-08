11c48c67b7118967208256ddaf1a18b80cbc3903
Borislav04 <borkolaka@gmail.com>
2025-03-08 20:11:17 +0200

    Create Student.java
    Добавен клас Student със свойства и конструктор.

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public String name;
+    public int facultyNumber;
+    public int year;
+    public String program;
+    public String group;
+    public String status;
+
+public Student(String name, int facultyNumber, int year, String program, String group) {
+        this.name = name;
+        this.facultyNumber = facultyNumber;
+        this.year = year;
+        this.program = program;
+        this.group = group;
+        this.status = "enrolled";
+    }
+}
