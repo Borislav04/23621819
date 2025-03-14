1a18c61064accf9c987605682c4b72c7ee3d81d0
Borislav04 <borkolaka@gmail.com>
2025-03-14 18:41:02 +0200

    Create Course.java
    Добавен клас Course за управление на дисциплините.

---
+++ Course.java
@@ -0,0 +1,11 @@
+public class Course {
+    public String name;
+    public String type;
+    public int year;
+
+    public Course(String name, String type, int year) {
+        this.name = name;
+        this.type = type;
+        this.year = year;
+    }
+}
