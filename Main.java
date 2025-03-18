7e1397930dd107477b49b51cbc64213be7e5e746
Borislav04 <borkolaka@gmail.com>
2025-03-18 18:45:21 +0200

    Update Main.java
    Актуализация на клас Main с тестове за записване на студенти и дисциплини чрез University

--- Main.java
+++ Main.java
@@ -1,28 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Иван Иванов", 12345, 1, "Информатика", "Група A");
-        Student student2 = new Student("Мария Петрова", 67890, 1, "Математика", "Група B");
+        University university = new University();
+
+        university.enrollStudent("Иван Иванов", 12345, "Информатика", "Група A");
+        university.enrollStudent("Мария Петрова", 67890, "Математика", "Група B");
 
         Course course1 = new Course("Програмиране", "задължителна", 1);
         Course course2 = new Course("Математика", "задължителна", 1);
 
-        student1.addCourse(course1);
-        student2.addCourse(course2);
+        university.enrollInCourse(12345, course1);
+        university.enrollInCourse(67890, course2);
 
-        System.out.println("Информация за студентите:");
-        printStudentInfo(student1);
-        printStudentInfo(student2);
-    }
-
-    public static void printStudentInfo(Student student) {
-        System.out.println("Студент: " + student.name);
-        System.out.println("Факултетен номер: " + student.facultyNumber);
-        System.out.println("Специалност: " + student.program);
-        System.out.println("Група: " + student.group);
-        System.out.println("Дисциплини:");
-        for (Course course : student.courses) {
-            System.out.println("  - " + course.name + " (" + course.type + ", курс " + course.year + ")");
+        Student student = university.findStudentByFacultyNumber(12345);
+        if (student != null) {
+            System.out.println("Намерен студент: " + student.name);
+        } else {
+            System.out.println("Студентът не е намерен.");
         }
-        System.out.println();
     }
 }
