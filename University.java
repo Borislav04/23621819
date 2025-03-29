77c098608eb1bbe747fbce2ec30ebf6739ba412a
Borislav04 <borkolaka@gmail.com>
2025-03-29 17:48:34 +0200

    Update University.java
    Добавих възможност за добавяне на оценки, промяна на данни за студент и справка за записани дисциплини и оценки.

--- University.java
+++ University.java
@@ -24,12 +24,69 @@ public class University {
     }
 
     public void enrollInCourse(int facultyNumber, Course course) {
-    Student student = findStudentByFacultyNumber(facultyNumber);
-    if (student != null) {
-        student.addCourse(course);
-        System.out.println("Дисциплината " + course.name + " е записана за студент " + student.name);
-    } else {
-        System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+        Student student = findStudentByFacultyNumber(facultyNumber);
+        if (student != null) {
+            student.addCourse(course);
+            System.out.println("Дисциплината " + course.name + " е записана за студент " + student.name);
+        } else {
+            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+        }
     }
-}
+
+    public void addGrade(int facultyNumber, String courseName, double grade) {
+        Student student = findStudentByFacultyNumber(facultyNumber);
+        if (student != null) {
+            for (Course course : student.courses) {
+                if (course.name.equals(courseName)) {
+                    course.setGrade(grade);
+                    System.out.println("Оценка " + grade + " добавена по " + courseName);
+                    return;
+                }
+            }
+            System.out.println("Дисциплината " + courseName + " не е намерена.");
+        } else {
+            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+        }
+    }
+
+    public void changeStudentData(int facultyNumber, String option, String value) {
+        Student student = findStudentByFacultyNumber(facultyNumber);
+        if (student != null) {
+            switch (option) {
+                case "курс":
+                    student.year = Integer.parseInt(value);
+                    System.out.println("Курсът на студент " + student.name + " е променен на " + value);
+                    break;
+                case "специалност":
+                    student.program = value;
+                    System.out.println("Специалността на студент " + student.name + " е променена на " + value);
+                    break;
+                case "група":
+                    student.group = value;
+                    System.out.println("Групата на студент " + student.name + " е променена на " + value);
+                    break;
+                default:
+                    System.out.println("Невалидна опция за промяна.");
+            }
+        } else {
+            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+        }
+    }
+
+    public void printStudentReport(int facultyNumber) {
+        Student student = findStudentByFacultyNumber(facultyNumber);
+        if (student != null) {
+            System.out.println("Информация за студент:");
+            System.out.println("Име: " + student.name);
+            System.out.println("Факултетен номер: " + student.facultyNumber);
+            System.out.println("Специалност: " + student.program);
+            System.out.println("Група: " + student.group);
+            System.out.println("Дисциплини:");
+            for (Course course : student.courses) {
+                System.out.println("  - " + course.name + " (" + course.type + ", курс " + course.year + "): " + (course.grade > 0 ? course.grade : "няма оценка"));
+            }
+        } else {
+            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+        }
+    }
 }
