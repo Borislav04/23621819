c4f6bf936177d3cc943c1ccdb2877341702a69b9
Borislav04 <borkolaka@gmail.com>
2025-03-29 17:42:09 +0200

    Update Course.java
    Добавих grade като поле в Course, за да съхранявам оценки. Сложих го да се инициализира с 0.0 по подразбиране и добавих getter и setter за него.

--- Course.java
+++ Course.java
@@ -2,10 +2,20 @@ public class Course {
     public String name;
     public String type;
     public int year;
+    public double grade;
 
     public Course(String name, String type, int year) {
         this.name = name;
         this.type = type;
         this.year = year;
+        this.grade = 0.0;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
     }
 }
