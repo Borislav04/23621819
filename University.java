71fb442f87a9defe7ecb0ea3abc1707effbf5fb6
Borislav04 <borkolaka@gmail.com>
2025-04-17 01:46:43 +0300

    Update University.java
    обновен клас University с поддръжка на FileManager и опростена логика за студенти

--- University.java
+++ University.java
@@ -1,98 +1,49 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class University implements Serializable {
-    private static final long serialVersionUID = 1L;
-
     private List<Student> students;
+    private List<Course> courses;
+    private transient FileManager fileManager;
 
     public University() {
         this.students = new ArrayList<>();
+        this.courses = new ArrayList<>();
     }
 
-    public void enrollStudent(String name, int facultyNumber, String program, String group) {
-        Student student = new Student(name, facultyNumber, 1, program, group);
+    public void setFileManager(FileManager fileManager) {
+        this.fileManager = fileManager;
+    }
+
+    public void enrollStudent(Student student) {
         students.add(student);
-        System.out.println("Студентът " + name + " е записан успешно.");
+        saveData();
     }
 
-    public Student findStudentByFacultyNumber(int facultyNumber) {
-        for (Student student : students) {
-            if (student.facultyNumber == facultyNumber) {
-                return student;
-            }
-        }
-        return null;
+    public Optional<Student> getStudent(String facultyNumber) {
+        return students.stream()
+                .filter(s -> s.getFacultyNumber().equals(facultyNumber))
+                .findFirst();
     }
 
-    public void enrollInCourse(int facultyNumber, Course course) {
-        Student student = findStudentByFacultyNumber(facultyNumber);
-        if (student != null) {
-            student.addCourse(course);
-            System.out.println("Дисциплината " + course.name + " е записана за студент " + student.name);
-        } else {
-            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
-        }
+    public void interruptStudent(String facultyNumber) {
+        getStudent(facultyNumber).ifPresent(Student::interrupt);
+        saveData();
     }
 
-    public void addGrade(int facultyNumber, String courseName, double grade) {
-        Student student = findStudentByFacultyNumber(facultyNumber);
-        if (student != null) {
-            for (Course course : student.courses) {
-                if (course.name.equals(courseName)) {
-                    course.setGrade(grade);
-                    System.out.println("Оценка " + grade + " добавена по " + courseName);
-                    return;
-                }
-            }
-            System.out.println("Дисциплината " + courseName + " не е намерена.");
-        } else {
-            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
+    private void saveData() {
+        if (fileManager != null) {
+            fileManager.save(this);
         }
     }
-
-    public void changeStudentData(int facultyNumber, String option, String value) {
-        Student student = findStudentByFacultyNumber(facultyNumber);
-        if (student != null) {
-            switch (option) {
-                case "курс":
-                    student.year = Integer.parseInt(value);
-                    System.out.println("Курсът на студент " + student.name + " е променен на " + value);
-                    break;
-                case "специалност":
-                    student.program = value;
-                    System.out.println("Специалността на студент " + student.name + " е променена на " + value);
-                    break;
-                case "група":
-                    student.group = value;
-                    System.out.println("Групата на студент " + student.name + " е променена на " + value);
-                    break;
-                default:
-                    System.out.println("Невалидна опция за промяна.");
-            }
-        } else {
-            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
-        }
-    }
-
-    public void printStudentReport(int facultyNumber) {
-        Student student = findStudentByFacultyNumber(facultyNumber);
-        if (student != null) {
-            System.out.println("Информация за студент:");
-            System.out.println("Име: " + student.name);
-            System.out.println("Факултетен номер: " + student.facultyNumber);
-            System.out.println("Специалност: " + student.program);
-            System.out.println("Група: " + student.group);
-            System.out.println("Дисциплини:");
-            for (Course course : student.courses) {
-                System.out.println("  - " + course.name + " (" + course.type + ", курс " + course.year + "): " + (course.grade > 0 ? course.grade : "няма оценка"));
-            }
-        } else {
-            System.out.println("Студент с факултетен номер " + facultyNumber + " не е намерен.");
-        }
-    }
+    
     public List<Student> getStudents() {
         return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
