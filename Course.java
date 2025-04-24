7f7ba8a080c4957eca940f771acd3a098ed52d1b
Borislav04 <borkolaka@gmail.com>
2025-04-24 19:58:49 +0300

    Update Course.java
    - Заменен enum CourseType със String тип
    - Добавена информация за курс/година на обучение
    - Добавено поле за оценка и съответен setter с валидация
    - Добавен копи-конструктор
    - Разширен toString() метод с допълнителна информация

--- Course.java
+++ Course.java
@@ -1,39 +1,59 @@
 import java.io.Serializable;
 
 public class Course implements Serializable {
-    public enum CourseType {
-        MANDATORY, ELECTIVE
-    }
+    private static final long serialVersionUID = 1L;
+
 
     private String name;
-    private CourseType type;
-    private int credits; // Само за избираеми дисциплини
+    private String type;
+    private int year;
+    private int credits;
+    private double grade;
 
-    public Course(String name, CourseType type) {
-        this(name, type, type == CourseType.ELECTIVE ? 3 : 0);
+    public Course(String name, String type, int year) {
+        this(name, type, year, type.equals("избираема") ? 3 : 0);
     }
 
-    public Course(String name, CourseType type, int credits) {
+    public Course(String name, String type, int year, int credits) {
         this.name = name;
         this.type = type;
-        this.credits = type == CourseType.ELECTIVE ? credits : 0;
+        this.year = year;
+        this.credits = credits;
+        this.grade = 0.0;
+    }
+    public Course(Course other) {
+        this.name = other.name;
+        this.type = other.type;
+        this.year = other.year;
+        this.credits = other.credits;
+        this.grade = other.grade;
     }
 
-    // Getters
     public String getName() {
-        return name;
+        return name; 
     }
-
-    public CourseType getType() {
-        return type;
+    public String getType() {
+        return type; 
     }
-
+    public int getYear() {
+        return year; 
+    }
     public int getCredits() {
         return credits;
+    }
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        if (grade >= 2.0 && grade <= 6.0) {
+            this.grade = grade;
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("%s (%s)", name, type);
+        return String.format("%s (%s, %d курс) - %s, Кредити: %d",
+                name, type, year, grade > 0 ? "Оценка: " + grade : "Невзет", credits);
     }
 }
